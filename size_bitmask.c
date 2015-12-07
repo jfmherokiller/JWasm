38ee289550b4d7250b6df02b79cc649af5aa28c1
jfm meyers <peterpan0413@live.com>
2015-12-07 13:41:22 -0600

    uploaded the missing files

---
+++ size_bitmask.c
@@ -0,0 +1,39 @@
+/*For a size of n, return a bitmask such that n elements can be enumarated*/
+/*If you need to "trim" so alignment fits to a number of bits, reverse the*/
+/*mask returned by this function:                                         */
+/*                                                                        */
+/*uint32_t positive_mask = size_bitmask(size);                            */
+/*uint32_t negative_mask = size_bitmask(size) ^ 0xFFFFFFFFU;              */
+__int32  size_bitmask(__int32 size) {
+	//Size should not be Zero
+	//assert(size > 0);
+	//...But if size is Zero, the bitmask is 0xFFFFFFFF
+	if (size == 0) {
+		return 0xFFFFFFFFU;
+	}
+
+	//To generate the mask we set every bit below the first set bit, MSB first.
+
+	__int8 set_bit_index;
+	//Search the first set bit
+	for (__int8 i = 0; i < 32; ++i) {
+		//For each bit
+		set_bit_index = 31 - i;
+		//If set
+		if (size & (1 << set_bit_index)) {
+			break; //We found our first set bit
+		}
+		//Otherwise
+		else {
+			; //Do nothing
+		}
+	}
+
+	//Now, set every size bit below bit_index to generate the mask
+	for (__int8 i = 0; i < set_bit_index; ++i) {
+		size = size | (1 << i);
+	}
+
+	//Return the mask
+	return size;
+}
